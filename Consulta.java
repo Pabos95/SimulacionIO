8453648b32cf55fe8ba5de6b708e981776b27df8
Pabos95 <pabloagc95@gmail.com>
2018-05-29 22:32:34 -0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Consulta.java
    #	Simulacion.java

--- Consulta.java
+++ Consulta.java
@@ -1,8 +1,24 @@
+package SimulacionIO;
 public class Consulta{
+    bool activo;
+    float tiempoCola;
+    float tiempoTotal;
+
     public enum tipoConsulta{
         select, update, join, ddl;
     }
-    public Consulta(){
-
+    public Consulta(float numAelatorio){
+     if(numAelatorio >= 0.0 && umAealatorio <= 0.30){
+         tipoConsulta = select
+     }
+     else if(numAelatorio >= 0.31 && numAelatorio <= 0.55){
+         tipoConsulta = update;
+     }
+     else if(numAelatorio >= 0.56 && numAelatorio <= 0.80){
+         tipoConsulta = join;
+     }
+     else if(numAelatorio >= 0.56 && numAelatorio <= 1.00){
+         tipoConsulta = ddl;
+     }
     }
 }
