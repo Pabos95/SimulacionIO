b5271c78f2e40ab44a288f4c497d0fce04605848
Pablo E. Aguilar <pabloagc95@gmail.com>
2018-06-06 16:35:24 -0600

    Variables de clase agregadas

--- Simulacion.java
+++ Simulacion.java
@@ -1,5 +1,16 @@
 package SimulacionIO;
 class Simulacion{
+  int iteracionActual;
+  int tiempoMaximo;  //tiempo maximo de la simulacion
+  int k; // numero maximo de conexiones concurrentes que puede administrar el sistema
+  int n; //numero de procesos disponibles para el procesamiento de consultas. concurrentes que puede manejar el sistema.
+  int p; //numero de procesos disponibles para la ejecución de transacciones
+  int m; //numero de procesos disponibles para la ejecución de consultas
+  int t; //cantidad de segundos para el timeout de las conexiones
+  bool modoLento// true si la conexión está en modoLento y falso en caso contrario
+  public Simulacion(int numCorridas,int numConexionesConcurrentesMaximo){
+
+  }
   public void ejecutarSimulacion(){
 
   }
