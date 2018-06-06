9ef440a38d1956e09b08ffc577b6a4b9c9818c21
Pabos95 <pabloagc95@gmail.com>
2018-06-06 16:35:24 -0600

    Metodo ejecturaSimulacion  modificado

--- Simulacion.java
+++ Simulacion.java
@@ -1,6 +1,20 @@
 package SimulacionIO;
 class Simulacion{
+  int iteracionActual;
+  int numIteraciones;
+  int tiempoMaximo;  //tiempo maximo de la simulacion
+  int k; // numero maximo de conexiones concurrentes que puede administrar el sistema
+  int n; //numero de procesos disponibles para el procesamiento de consultas. concurrentes que puede manejar el sistema.
+  int p; //numero de procesos disponibles para la ejecución de transacciones
+  int m; //numero de procesos disponibles para la ejecución de consultas
+  int t; //cantidad de segundos para el timeout de las conexiones
+  bool modoLento// true si la conexión está en modoLento y falso en caso contrario
+  public Simulacion(int numCorridas,int numConexionesConcurrentesMaximo){
+   iteracionActual = 0;
+  }
   public void ejecutarSimulacion(){
-
+  while(iteracionActual <= numIteraciones){
+    
+  }
   }
 }
