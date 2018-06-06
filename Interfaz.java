5b3b8be90ea5441bc2b9d32677dd6a6f28c8367a
Pablo E. Aguilar <pabloagc95@gmail.com>
2018-06-06 12:21:13 -0600

    Campos de texto agregados

--- Interfaz.java
+++ Interfaz.java
@@ -2,14 +2,14 @@ package SimulacionIO;
 import javax.swing.*;
 public class Interfaz extends JFrame{
     public Interfaz(){
-
+        numeroCorridas = new JTextField();
+        modoLento = new JTextField();
     }
     public static void main(String[] args) {
         Interfaz miAplicacion = new Interfaz();
         miAplicacion.setBounds(10,10,400,400);
         miAplicacion.pack();
         miAplicacion.setVisible(true);
-        numeroCorridas = new JTextField();
-        modoLento = new JTextField();
+
     }
 }
