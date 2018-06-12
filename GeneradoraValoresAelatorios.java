8f28b1e90050b65587a3c92a5e9bf1ff040442eb
Pablo E. Aguilar <pabloagc95@gmail.com>
2018-06-12 17:29:40 -0600

    prueba

---
+++ GeneradoraValoresAelatorios.java
@@ -0,0 +1,14 @@
+import Random.rand;
+public class GeneradoraValoresAelatorios(){
+    Random r;
+    public GeneradoraValoresAelatorios(){
+        r = new Random(Math.random() * 10000);
+        }
+public float generarValorDistribuicionNormal(){ //utiliza el método de convolución
+         float nuevoValor = 0;
+         return nuevoValor;
+        }
+public float generarNumeroAelatorio(){ //genera un número entre 0 y 1
+           return double.toFloat(r.nextDouble());
+        }
+        }
