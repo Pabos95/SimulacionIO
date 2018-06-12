78d80f5eab112f6351f41be3ff91043017e21272
Pablo E. Aguilar <pabloagc95@gmail.com>
2018-06-12 17:49:43 -0600

    prueba

--- Interfaz.java
+++ Interfaz.java
@@ -281,7 +281,7 @@ import javax.swing.*;
         /**
          * @param args the command line arguments
          */
-        public static void main(String args[]) {
+        fun main(args: Array<String>) {
                 /* Set the Nimbus look and feel */
                 //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
                 /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
