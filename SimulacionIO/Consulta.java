d33549a975421a2f5bc3961d2753d658c327fb76
Pablo E. Aguilar <pabloagc95@gmail.com>
2018-06-14 11:33:31 -0600

    Método para generar un valor aelatorio con distribuición uniforme agregado.

---
+++ SimulacionIO/Consulta.java
@@ -0,0 +1,25 @@
+package SimulacionIO;
+public class Consulta{
+    public enum tipoConsulta{
+        select, update, join, ddl;
+    }
+    boolean activo;
+    double tiempoCola;
+    double tiempoTotal;
+    tipoConsulta tConsulta;
+    public Consulta(double numAelatorio){
+     if(numAelatorio >= 0.0 && numAelatorio <= 0.30){
+         tConsulta = tipoConsulta.select;
+     }
+     else if(numAelatorio >= 0.31 && numAelatorio <= 0.55){
+         tConsulta = tipoConsulta.update;
+     }
+     else if(numAelatorio >= 0.56 && numAelatorio <= 0.80){
+         tConsulta = tipoConsulta.join;
+     }
+     else if(numAelatorio >= 0.56 && numAelatorio <= 1.00){
+         tConsulta = tipoConsulta.ddl;
+     }
+    }
+   
+    }
