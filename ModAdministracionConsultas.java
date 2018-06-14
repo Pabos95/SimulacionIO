a066cccf0aa7b2326c67693bf0bad9e1e7d2e721
Pabos95 <pabloagc95@gmail.com>
2018-06-14 11:32:18 -0600

    Delete ModAdministracionConsultas.java

--- ModAdministracionConsultas.java
+++
@@ -1,9 +0,0 @@
-package Modulo;
-import java.util.*;
-import SimulacionIO.*;
-import Estadisticos.*;
-class ModAdministracionConsultas extends Modulo{
-    int n;
-    int tamFinalColaP;
-    int tamActualColaP;
-}
