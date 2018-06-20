514fe960309a18ae1fd01472c8b7a1a6320c799f
Pabos95 <pabloagc95@gmail.com>
2018-06-20 16:15:35 -0600

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import SimulacionIO.*;
+import Estadisticos.*;
+import Interfaz.*;
+impoort Modulo.*;
+public class Main {
+public static void main(String[] args) {
+  // do something here...
+}
+} 
