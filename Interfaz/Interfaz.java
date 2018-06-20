ff0d6fa7987a46bec8196eb71b293547e55e5d88
Pabos95 <pabloagc95@gmail.com>
2018-06-20 17:11:28 -0600

    Update Interfaz.java

--- Interfaz/Interfaz.java
+++ Interfaz/Interfaz.java
@@ -422,12 +422,12 @@ public class Interfaz extends javax.swing.JFrame  {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
+  /*  public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
+       
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
@@ -446,14 +446,14 @@ public class Interfaz extends javax.swing.JFrame  {
         }
         //</editor-fold>
 
-        /* Create and display the form */
+        /* Create and display the form 
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new Interfaz().setVisible(true);
             }
         });
-    }
-
+    }*/
+//Comenté el main porque ahora el main va a estar en una clase aparte
     // Variables declaration - do not modify                     
     private javax.swing.JTextField Duracion;
     private javax.swing.JTextArea MostrarListaEventos;
