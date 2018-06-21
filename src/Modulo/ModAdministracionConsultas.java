a2e5258e39bcca0415210ca38283dd97c9365abe
Ana Laura <analaura19mongesoto@gmail.com>
2018-06-21 02:36:22 -0600

    Cambios en ModConsulta
    Si vamos a manejar ModConsulta con dos llegadas diferentes, también
    ocupamos una cola diferente para las sentencias que se van a ejecutar
    después de pasar por el modulo Transacciones que sería List<Consulta>
    colaEjecutar. También inicialicé las listas en el constructor del
    modulo.

--- src/Modulo/ModAdministracionConsultas.java
+++ src/Modulo/ModAdministracionConsultas.java
@@ -5,16 +5,20 @@ import Estadisticos.*;
 public class ModAdministracionConsultas extends Modulo{
     int n;
     int m;
-	double timeSalida; //Tiempo de salida después de realizar las etapas de validación 
+    double timeSalida; //Tiempo de salida después de realizar las etapas de validación 
     int tamFinalColaP;
     int tamActualColaP;
-	GeneradoraValoresAelatorios gen;
+    GeneradoraValoresAelatorios gen;
+    List<Consulta> colaEjecutar; //Cola para cuando la llegada venga de Modulo de transacciones
+    int sentenciasEjecucion; //Contador de cuantas sentencias están siendo ejecutadas
 
-	public ModAdministracionConsultas(int tam, int tam2){
-	    n = tam;
-	    m = tam2;
+    public ModAdministracionConsultas(int tam, int tam2){
         gen = new GeneradoraValoresAelatorios();
+        colaConsultas = new ArrayList<>(); //Esto podría ser new LinkedList<>() que tiene los mismos métodos que Priority Queue y disciplina FIFO
+        colaEjecutar = new ArrayList<>();
         timeSalida = 0;
+        sentenciasEjecucion = 0;
+        
     }
     
     public void agregarConsulta (Consulta c){ //Se agrega la consulta a una cola con disciplina FIFO
@@ -23,7 +27,7 @@ public class ModAdministracionConsultas extends Modulo{
     }
 
     @Override
-	public void procesarLlegada(Consulta consulta) {
+    public void procesarLlegada(Consulta consulta) {
         if(!consulta.getMuerto()){//Si está muerto significa que no fue admitido en el módulo pasado
             if (consultasActuales < n){ //Hay al menos un proceso disponible para atender la consulta que viene
                 //Se procesan consultas haciendo etapas de validación
@@ -41,10 +45,9 @@ public class ModAdministracionConsultas extends Modulo{
             } else {
                 //Se agrega a la cola
                 agregarConsulta(consulta);
-            }
-        
+            }                  
         }
-
+        timeSalida = 0; //Se resetea
     }
 
     @Override
@@ -52,7 +55,27 @@ public class ModAdministracionConsultas extends Modulo{
 
     }
 
-	public void procesarLlegada(Consulta consulta, double B) { //Cuando la llegada viene del modulo de Transacciones
+    public void procesarLlegada(Consulta consulta, double B) { //Cuando la llegada viene del modulo de Transacciones
+        //Puede manejar m consultas en ejecución
+        if (sentenciasEjecucion < m){
+            switch(consulta.getTConsulta()){           
+                case ddl:                         
+                    timeSalida = consulta.getTiempoActual() + 0.5; //Procesar ejecución de DDL                                        
+                break;               
+                case update:
+                    timeSalida = consulta.getTiempoActual() + 1;                    
+                break;               
+                case join:
+                    timeSalida = consulta.getTiempoActual() + (Math.pow(B, 2) * 0.001); //Recordar que B^2 son milisegundos por lo que hay que pasarlos a segundos                
+                break;               
+                case select:
+                    timeSalida = consulta.getTiempoActual() + (Math.pow(B, 2) * 0.001);
+                break;                   
+            }
+            
+        } else {
+            colaEjecutar.add(consulta);
+        }
 
     }
 	
