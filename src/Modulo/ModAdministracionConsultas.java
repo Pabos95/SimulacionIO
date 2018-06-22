4bea83a03218683962cc1656012383e89b70f04d
Ana Laura <analaura19mongesoto@gmail.com>
2018-06-22 00:01:30 -0600

    mini cambio en modConsultas

--- src/Modulo/ModAdministracionConsultas.java
+++ src/Modulo/ModAdministracionConsultas.java
@@ -5,7 +5,7 @@ import Estadisticos.*;
 public class ModAdministracionConsultas extends Modulo{
     int n;
     int m;
-    double timeSalida; //Tiempo de salida después de realizar las etapas de validación 
+    double timeEjecucion; //Tiempo de ejecución después de realizar las etapas de validación 
     int tamFinalColaP;
     int tamActualColaP;
     GeneradoraValoresAelatorios gen;
@@ -18,7 +18,7 @@ public class ModAdministracionConsultas extends Modulo{
         gen = new GeneradoraValoresAelatorios();
         colaConsultas = new ArrayList<Consulta>(); //Esto podría ser new LinkedList<>() que tiene los mismos métodos que Priority Queue y disciplina FIFO
         colaEjecutar = new ArrayList<Consulta>();
-        timeSalida = 0;
+        timeEjecucion = 0;
         sentenciasEjecucion = 0;
         
     }
@@ -96,20 +96,20 @@ public class ModAdministracionConsultas extends Modulo{
                                                     //Viene del módulo adm de procesos
 
         //Se procesan consultas haciendo etapas de validación
-        timeSalida = 0; //Se resetea
-        timeSalida = consulta.getTiempoActual() + 1/10; //Duración de validación Léxica
-        timeSalida +=  gen.generarValorDistribuicionUniforme(0.0, 1.0); //Duración de validación Sintáctica
-        timeSalida +=  gen.generarValorDistribuicionUniforme(0.0, 2.0); //Duración de validación Semántica
-        timeSalida +=  gen.generarValorDistribuicionExponencial(0.7); //Verificación de permisos
+        timeEjecucion = 0; //Se resetea
+        timeEjecucion = 1/10; //Duración de validación Léxica
+        timeEjecucion +=  gen.generarValorDistribuicionUniforme(0.0, 1.0); //Duración de validación Sintáctica
+        timeEjecucion +=  gen.generarValorDistribuicionUniforme(0.0, 2.0); //Duración de validación Semántica
+        timeEjecucion +=  gen.generarValorDistribuicionExponencial(0.7); //Verificación de permisos
         //Optimización de consultas
         if ((consulta.getTConsulta().compareTo(Consulta.tipoConsulta.ddl) == 0) ||  //No son de read-only
                 (consulta.getTConsulta().compareTo(Consulta.tipoConsulta.update) == 0)){  //No son de read-only
-            timeSalida +=  1/4;
+            timeEjecucion +=  1/4;
         } else { //Son read-only
-            timeSalida +=  0.1;
+            timeEjecucion +=  0.1;
         }
-        consulta.setTiempoVida(consulta.getTiempoVida() + timeSalida);
-        consulta.setTiempoActual(consulta.getTiempoActual() + timeSalida);
+        consulta.setTiempoVida(consulta.getTiempoVida() + timeEjecucion);
+        consulta.setTiempoActual(consulta.getTiempoActual() + timeEjecucion);
         consulta.setTipoEvento(Evento.tipoEvento.llegadaModuloTransacciones);
 
         if(colaConsultas.isEmpty()){
@@ -125,23 +125,23 @@ public class ModAdministracionConsultas extends Modulo{
         if (sentenciasEjecucion < m){
             switch(consulta.getTConsulta()){           
                 case ddl:                         
-                    timeSalida = consulta.getTiempoActual() + 0.5; //Procesar ejecución de DDL                                        
+                    timeEjecucion = consulta.getTiempoActual() + 0.5; //Procesar ejecución de DDL                                        
                 break;               
                 case update:
-                    timeSalida = consulta.getTiempoActual() + 1;                    
+                    timeEjecucion = consulta.getTiempoActual() + 1;                    
                 break;               
                 case join:
-                    timeSalida = consulta.getTiempoActual() + (Math.pow(B, 2) * 0.001); //Recordar que B^2 son milisegundos por lo que hay que pasarlos a segundos                
+                    timeEjecucion = consulta.getTiempoActual() + (Math.pow(B, 2) * 0.001); //Recordar que B^2 son milisegundos por lo que hay que pasarlos a segundos                
                 break;               
                 case select:
-                    timeSalida = consulta.getTiempoActual() + (Math.pow(B, 2) * 0.001);
+                    timeEjecucion = consulta.getTiempoActual() + (Math.pow(B, 2) * 0.001);
                 break;                   
             }
 
         } else {
             agregarConsultaEjecutar(consulta);
         }
-        timeSalida  = 0;
+        timeEjecucion  = 0;
     }
 	
     @Override
