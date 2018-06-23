55ab2883c1d131909ea86fd33b04585b87c2fa65
Pablo E. Aguilar <pabloagc95@gmail.com>
2018-06-23 14:23:10 -0600

    Ya la tercera ventana se carga bien cuando se marcan los parametros de la simulacion, tuve que crear una clase intermedia (en la que se guardan los numeros que el usuario puso en los JTextField para luego llamar al constructor de simulación), porque cuando mandaba la simulacion en el evento de Listo era cuando no se me cargaba bien.

--- src/SimulacionIO/Simulacion.java
+++ src/SimulacionIO/Simulacion.java
@@ -6,6 +6,29 @@ import Interfaz.*;
 import javafx.util.Pair;
 
 
+public class Simulacion implements Runnable {       
+    ModAdministracionClientes modAdminClientes;
+    ModAdministracionConsultas modAdminConsultas;
+    ModAdministracionProcesos modAdminProcesos;
+    ModAdministracionTransacciones modAdminTransacciones;
+    int iteracionActual;
+    double tiempoMaximo;  //tiempo maximo de la simulacion
+    int cantidadCorridas;// cantidad de corridas que se van a ejecutar de la simulación
+    int k; // numero maximo de conexiones concurrentes que puede administrar el sistema
+    int n; //numero de procesos disponibles para el procesamiento de consultas. concurrentes que puede manejar el sistema.
+    int p; //numero de procesos disponibles para la ejecución de transacciones
+    int m; //numero de procesos disponibles para la ejecución de consultas
+    int t; //cantidad de segundos para el timeout de las conexiones
+    double tiempoActual;
+    boolean modoLento;// true si la conexión está en modoLento y falso en caso contrario
+
+    public static List<Consulta> listaEventos; //Contiene la lista de los eventos por ejecutar
+    ArrayList <Consulta> consultas; //almacena las consultas de la simulacion para al final de una corrida poder calcular el tiempo de vida promedio
+    ArrayList <EstadisticosModulo> estadisticasModulo;
+    ArrayList <EstadisticosIteracion> estadisticosIteracion;
+    EstadisticosGenerales eg;
+    GeneradoraValoresAelatorios gen;
+    VentanaEjecucion ventana;
 public class Simulacion implements Runnable {    
   ModAdministracionClientes modAdminClientes;
   ModAdministracionConsultas modAdminConsultas;
